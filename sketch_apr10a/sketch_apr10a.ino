51421b666e82be72975a5e74a1320410cfe67db9
matteosaetti <matteosaett@gmail.com>
2024-04-13 14:19:18 +0200

    Update sketch_apr10a.ino
    add code for force sensor

--- sketch_apr10a/sketch_apr10a.ino
+++ sketch_apr10a/sketch_apr10a.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#define FORCE_SENSOR_PIN 35 // ESP32 pin GIOP35 (ADC1): 
 
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int analogReading = analogRead(FORCE_SENSOR_PIN);
 
+  Serial.print("The force sensor value = ");
+  Serial.print(analogReading); // print the raw analog reading
+
+  if (analogReading < 10)       // from 0 to 9
+    Serial.println(" -> no pressure");
+  else if (analogReading < 200) // from 10 to 199
+    Serial.println(" -> light touch");
+  else if (analogReading < 500) // from 200 to 499
+    Serial.println(" -> light squeeze");
+  else if (analogReading < 800) // from 500 to 799
+    Serial.println(" -> medium squeeze");
+  else // from 800 to 1023
+    Serial.println(" -> big squeeze");
+
+  delay(1000);
 }
