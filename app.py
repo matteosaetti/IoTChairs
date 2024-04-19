d45177784781ecd1577cea8d8ec74d854e3dc94c
Matteo <matteo.saetti@zanasicoding.com>
2024-04-19 14:05:38 +0200

    add html, css, js first file

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
 # Crea un'applicazione Flask
-app = Flask(__name__)
+template_dir = os.path.abspath("web/templates")
+static_dir   = os.path.abspath("web/static")
+app = Flask(__name__, template_folder=template_dir,static_url_path='', static_folder=static_dir)
 
 # Definisci una rotta per la homepage
 @app.route('/')
 def home():
-    return "Benvenuto alla nostra pagina web!"
+    return render_template("index.html")
 
 # Avvia il server Flask
 if __name__ == '__main__':
