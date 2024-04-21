8889d1418b2903f282e88978e902fe00bc6112fd
matteosaetti <matteosaett@gmail.com>
2024-04-21 20:12:05 +0200

    add settings.html & linking

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import os
 from flask import Flask, render_template
 
-# Crea un'applicazione Flask
 template_dir = os.path.abspath("web/templates")
 static_dir   = os.path.abspath("web/static")
 app = Flask(__name__, template_folder=template_dir,static_url_path='', static_folder=static_dir)
 
-# Definisci una rotta per la homepage
 @app.route('/')
 def home():
     return render_template("index.html")
 
-# Avvia il server Flask
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
