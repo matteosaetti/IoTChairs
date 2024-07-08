ce8234b874eed088c182b69fa3c54ac3efbe9d19
Matteo <matteo.saetti@zanasicoding.com>
2024-07-08 13:39:03 +0200

    adjust esp32 code

--- esp1_press/esp1_press.ino
+++ esp1_press/esp1_press.ino
@@ -1,38 +1,49 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const char* ssid = "nome wifi";
-const char* password = "password wifi";
+const char *ssid = "nome wifi";
+const char *password = "password wifi";
 
-const char* mqtt_server = "192.168.18.10";
-const char* clientID = "ESP32Client1";
-const char* topic = "sensor/pressure";
+const char *mqtt_server = "192.168.18.10";
+const char *clientID = "ESP32Client1";
+const char *topic = "sensor/pressure";
 
-const int pressureSensorPin = A0; 
+const int pressureSensorPin = A0;
 const int pressureThreshold = 500;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void connectWiFi() {
+void connectWiFi()
+{
   Serial.println("Connessione alla rete WiFi...");
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connessione in corso...");
   }
   Serial.println("Connessione WiFi stabilita");
 }
 
-void connectMQTT() {
+void callback(char *topic, byte *payload, unsigned int length)
+{
+}
+
+void connectMQTT()
+{
   client.setCallback(callback);
 
   Serial.print("Connessione al server MQTT ");
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("...");
-    if (client.connect(clientID)) {
+    if (client.connect(clientID))
+    {
       Serial.println("Connesso al server MQTT");
-    } else {
+    }
+    else
+    {
       Serial.print("Errore, stato di connessione: ");
       Serial.println(client.state());
       delay(2000);
@@ -40,26 +51,32 @@ void connectMQTT() {
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   connectWiFi();
   client.setServer(mqtt_server, 1883);
   connectMQTT();
 }
 
-void loop() {
-  if (!client.connected()) {
+void loop()
+{
+  if (!client.connected())
+  {
     connectMQTT();
   }
   client.loop();
 
   int pressureValue = analogRead(pressureSensorPin);
   String st = "";
-  if (pressureValue > pressureThreshold) {
+  if (pressureValue > pressureThreshold)
+  {
     st = String(topic) + "#1#1";
     client.publish(topic, st.c_str());
     Serial.println("1");
-  } else {
+  }
+  else
+  {
     st = String(topic) + "#1#0";
     client.publish(topic, st.c_str());
     Serial.println("0");
--- esp3_light_temp/esp3_light_temp.ino
+++ esp3_light_temp/esp3_light_temp.ino
@@ -1,81 +1,90 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-const char* ssid = "nome wifi";
-const char* password = "password wifi";
+const char *ssid = "nome wifi";
+const char *password = "password wifi";
 
-const char* mqtt_server = "192.168.18.10";
-const char* clientID = "ESP32Client2";
+const char *mqtt_server = "192.168.18.10";
+const char *clientID = "ESP32Client2";
 
-const char* topic_mode = "mode";
-const char* topic_light = "sensor/light";
-const char* topic_temp = "sensor/temp";
-const char* topic_b_light = "buttons/light";
-const char* topic_b_temp = "buttons/temp";
-const char* topic_set_light = "settings/light";
-const char* topic_set_temp = "settings/temp";
+const char *topic_mode = "mode";
+const char *topic_light = "sensor/light";
+const char *topic_temp = "sensor/temp";
+const char *topic_b_light = "buttons/light";
+const char *topic_b_temp = "buttons/temp";
+const char *topic_set_light = "settings/light";
+const char *topic_set_temp = "settings/temp";
 
 const int lightPin = 32;
 const int heatPin = 33;
 const int tempPin = A0;
 
-
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-
-float tempSet  = 25.0;
+float tempSet = 25.0;
 float lightSet = 300;
-bool lightOn   = false;
-bool heatOn    = false;
-bool mode      = false;
+bool lightOn = false;
+bool heatOn = false;
+bool mode = false;
 
-void callback(char* topic, byte* message, unsigned int length) {
+void callback(char *topic, byte *message, unsigned int length)
+{
   String message_s;
-  
-  for (int i = 0; i < length; i++) {
+
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)message[i]);
     message_s += (char)message[i];
   }
-  Serial.println("Msg on topic: "+topic+ " --> "+message_s);
+  Serial.println("Msg on topic: " + String(topic) + " --> " + message_s);
 
-  if (String(topic) == topic_mode) {
+  if (String(topic) == topic_mode)
+  {
     mode = message_s.toInt();
   }
-  else if (String(topic) == topic_set_light) {
+  else if (String(topic) == topic_set_light)
+  {
     lightSet = message_s.toInt();
   }
-  else if (String(topic) == topic_set_temp) {
+  else if (String(topic) == topic_set_temp)
+  {
     tempSet = message_s.toFloat();
   }
-  else if (String(topic) == topic_b_light) {
-    //char* split = strtok(message_s.toCharArray(), "#");
-    lightOn = (message_s.toInt == 1)
+  else if (String(topic) == topic_b_light)
+  {
+    // char* split = strtok(message_s.toCharArray(), "#");
+    lightOn = (message_s.toInt() == 1);
   }
-  else if (String(topic) == topic_b_temp) {
-    //char* split = strtok(message_s.toCharArray(), "#");
-    heatOn = (message_s.toInt == 1)
+  else if (String(topic) == topic_b_temp)
+  {
+    // char* split = strtok(message_s.toCharArray(), "#");
+    heatOn = (message_s.toInt() == 1);
   }
 }
 
-
-void connectWiFi() {
+void connectWiFi()
+{
   Serial.println("Connessione alla rete WiFi...");
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connessione in corso...");
   }
   Serial.println("Connessione WiFi stabilita");
 }
 
-void connectMQTT() {
+void connectMQTT()
+{
   client.setCallback(callback);
 
   Serial.print("Connessione al server MQTT ");
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("...");
-    if (client.connect(clientID)) {
+    if (client.connect(clientID))
+    {
       Serial.println("Connesso al server MQTT");
 
       client.subscribe(topic_mode);
@@ -83,8 +92,8 @@ void connectMQTT() {
       client.subscribe(topic_set_temp);
       client.subscribe(topic_b_light);
       client.subscribe(topic_b_temp);
-    } 
-    else 
+    }
+    else
     {
       Serial.print("Errore, stato di connessione: ");
       Serial.println(client.state());
@@ -93,7 +102,8 @@ void connectMQTT() {
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(lightPin, OUTPUT);
   pinMode(heatPin, OUTPUT);
@@ -107,20 +117,23 @@ void setup() {
   connectMQTT();
 }
 
-void loop() {
-  if (!client.connected()) {
+void loop()
+{
+  if (!client.connected())
+  {
     connectMQTT();
   }
   client.loop();
 
   int temp = analogRead(tempPin);
   int light = analogRead(lightPin);
-  //TODO calcolare bene la temp
-  Serial.println("temp: "+String(temp));
-  Serial.println("light: "+String(light));
+  // TODO calcolare bene la temp
+  Serial.println("temp: " + String(temp));
+  Serial.println("light: " + String(light));
 
-  if(!mode){
-    if(light < lightSet)
+  if (!mode)
+  {
+    if (light < lightSet)
     {
       digitalWrite(lightPin, HIGH);
     }
@@ -128,49 +141,39 @@ void loop() {
     {
       digitalWrite(lightPin, LOW);
     }
-  
-    if(temp < tempSet)
+
+    if (temp < tempSet)
     {
       digitalWrite(heatPin, HIGH);
     }
-    else 
+    else
     {
       digitalWrite(heatPin, LOW);
     }
   }
   else
   {
-    if(lightOn){
+    if (lightOn)
+    {
       digitalWrite(lightPin, HIGH);
     }
     else
     {
       digitalWrite(lightPin, LOW);
     }
-    if(heatOn)
+    if (heatOn)
     {
       digitalWrite(heatPin, HIGH);
     }
-    else 
+    else
     {
       digitalWrite(heatPin, LOW);
     }
   }
-  //client.publish(topic_light, lightOn, sizeof(lightOn));
-  //client.publish(topic_temp, heatOn, sizeOf(heatOn));
+  // client.publish(topic_light, lightOn, sizeof(lightOn));
+  // client.publish(topic_temp, heatOn, sizeOf(heatOn));
   client.publish(topic_light, String(light).c_str());
-  client.publish(topic_temp, Sreing(temp).c_str());
+  client.publish(topic_temp, String(temp).c_str());
 
   delay(500);
 }
-
-
-
-
-
-
-
-
-
-
-
